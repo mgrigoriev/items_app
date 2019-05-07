3e3fc0fabde2b224656dd9a8051e46c96f20f8a4
Mikhail Grigoriev <mgrigoriev@gmail.com>
2019-05-08 02:36:41 +0300

    Tweak dockerization

--- Docker/start.sh
+++ Docker/start.sh
@@ -3,7 +3,7 @@
 export RAILS_ENV=$1
 
 if [ $1 == 'production' ]; then
-  rails assets:precompile && rails db:migrate && rails server -b 0.0.0.0
+  rm -f tmp/pids/server.pid && rails assets:precompile && rails db:migrate && rails server -b 0.0.0.0
 else
   rails db:migrate && rails server -b 0.0.0.0
 fi
